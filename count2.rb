b53f646ed7d8cd1537bc1e4c89f23eeceefc3d6c
oupo <oupo.nejiki@gmail.com>
2012-12-21 14:36:41 +0900

    Counterという名前をPredictorに変更

--- count2.rb
+++ count2.rb
@@ -13,7 +13,7 @@ def main
   starters = (0...NUM_STARTERS).to_a
   prng = PRNG.new(rand(2**32))
   p prng
-  x = Counter.count(prng, starters)
+  x = RoughPredictor.predict(prng, starters)
   print_condition(x.to_a[0], starters)
 end
 
@@ -37,69 +37,69 @@ def print_condition(result, starters)
   end
 end
 
-class Counter
+class RoughPredictor
   def initialize(starters)
     @starters = starters
   end
 
-  def self.count(prng, starters)
-    new(starters).count(prng)
+  def self.predict(prng, starters)
+    new(starters).predict(prng)
   end
 
-  def count(prng)
-    count0(prng, [], [])
+  def predict(prng)
+    predict0(prng, [], [])
   end
 
   Result = Struct.new(:prng, :enemies, :skipped)
 
-  def count0(prng, enemies, skipped)
+  def predict0(prng, enemies, skipped)
     if enemies.length == NUM_BATTLES
       return Set.new([Result.new(prng, enemies, skipped)])
     end
     prev_enemy = enemies.last || @starters
     maybe_players = @starters + enemies[0..-2].flatten
     surely_players = []
-    results = OneEnemyCounter.count(prng, prev_enemy, maybe_players, surely_players)
+    results = OneEnemyPredictor.predict(prng, prev_enemy, maybe_players, surely_players)
     results.map {|result|
-      count0(result.prng, enemies + [result.chosen], skipped + [result.skipped])
+      predict0(result.prng, enemies + [result.chosen], skipped + [result.skipped])
     }.inject(:+)
   end
 end
 
-class OneEnemyCounter
+class OneEnemyPredictor
   def initialize(prev_enemy, maybe_players, surely_players)
     @prev_enemy = prev_enemy
     @maybe_players = maybe_players
     @surely_players = surely_players
   end
 
-  def self.count(prng, prev_enemy, maybe_players, surely_players)
-    new(prev_enemy, maybe_players, surely_players).count(prng)
+  def self.predict(prng, prev_enemy, maybe_players, surely_players)
+    new(prev_enemy, maybe_players, surely_players).predict(prng)
   end
 
-  def count(prng)
-    count0(prng, @surely_players, [])
+  def predict(prng)
+    predict0(prng, @surely_players, [])
   end
 
   Result = Struct.new(:prng, :chosen, :skipped)
 
   # 戻り値の中の異なるResultオブジェクトは必ず異なるchosenになる
-  def count0(prng, surely_players, chosen)
+  def predict0(prng, surely_players, chosen)
     if chosen.length == NUM_PARTY
        return Set.new([Result.new(prng, chosen, surely_players)])
     end
     prngp, x = prng.rand(NUM_ENTRIES)
     if (@prev_enemy + surely_players + chosen).include?(x)
       # 常にスキップ
-      count0(prngp, surely_players, chosen)
+      predict0(prngp, surely_players, chosen)
     elsif not @maybe_players.include?(x) or surely_players.length == NUM_PARTY
       # 常に採用
-      count0(prngp, surely_players, chosen + [x])
+      predict0(prngp, surely_players, chosen + [x])
     else
       # プレイヤーがxを持っていてスキップする場合
-      result1 = count0(prngp, surely_players + [x], chosen)
+      result1 = predict0(prngp, surely_players + [x], chosen)
       # 採用する場合
-      result2 = count0(prngp, surely_players, chosen + [x])
+      result2 = predict0(prngp, surely_players, chosen + [x])
 
       result1 + result2
     end

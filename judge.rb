e8b4bdd4616f329176fb2e2a83d2feb1bb21e741
oupo <oupo.nejiki@gmail.com>
2012-12-21 15:46:47 +0900

    デバッグのためにアイテムIDをわかりやすい名前に置換してみるなど

--- judge.rb
+++ judge.rb
@@ -9,6 +9,8 @@ NUM_STARTERS = 6
 NUM_PARTY = 3
 NUM_BATTLES = 7
 
+N = NUM_BATTLES
+
 # TODO 実際にありえるパスかどうかを判定する必要がある
 def main
   starters = (0...NUM_STARTERS).to_a
@@ -18,24 +20,59 @@ def main
   print_condition(x.to_a[0], starters)
 end
 
-# そのパスを通るために必要な条件を出力する
 def print_condition(result, starters)
-  p starters
-  pp result
-  maybe_players = []
-  before = starters
-  result.enemies.zip(result.skipped).each_with_index do |(enemy, skipped), i|
-    enemy.each do |entry|
-      if maybe_players.include?(entry)
-        puts "#{entry} ∉ player[#{i-1}]"
-      end
-    end
-    skipped.each do |entry|
-      puts "#{entry} ∈ player[#{i-1}]"
-    end
-    maybe_players += before
-    before = enemy
+  shop, gate = result_to_shopgate(result, starters)
+  (0..N - 2).each do |i|
+    puts "shop #{i}: #{shop[i]}"
+    puts "gate #{i+2}: #{gate[i+2]} : #{shop[i+2]}"
   end
+end
+
+def result_to_shopgate(result, starters)
+  shop = []
+  gate = []
+  shop[0] = starters
+  (1..N).each do |i|
+    shop[i] = result.enemies[i-1]
+  end
+  (2..N).each do |i|
+    gate[i] = result.skipped[i-1]
+  end
+  subst_item_id(shop, gate)
+end
+
+class Item
+  def initialize(name)
+    @name = name
+  end
+
+  def inspect
+    @name
+  end
+end
+
+def subst_item_id(shop, gate)
+  ref = {}
+  (0..N).each do |i|
+    shop[i].each do |item|
+      ref[item] ||= []
+      ref[item] << i if i <= N - 2
+    end
+  end
+  used = ref.values.each_with_object(Hash.new(0)) {|r, u| u[r] += 1 }
+  count = Hash.new(0)
+  name = {}
+  ref.each do |item, r|
+    n = r.join("")
+    if used[r] > 1 or n == ""
+      n += "_" + ("a".ord + count[r]).chr
+    end
+    name[item] = Item.new(n)
+    count[r] += 1
+  end
+  shop.each {|x| x.map!{|item| name[item] } }
+  gate.each {|x| x and x.map!{|item| name[item] } }
+  [shop, gate]
 end
 
 

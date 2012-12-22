544bc09ae421be5cfcb52445e7d85edde4219909
oupo <oupo.nejiki@gmail.com>
2012-12-22 19:22:20 +0900

    JudgeContextやassign_loop

--- judge.rb
+++ judge.rb
@@ -3,6 +3,7 @@ require "set"
 require "pp"
 require_relative "prng.rb"
 require_relative "rough-predictor.rb"
+require_relative "judge-context.rb"
 
 NUM_ENTRIES = 30
 NUM_STARTERS = 6
@@ -17,19 +18,24 @@ def main
   starters = (0...NUM_STARTERS).to_a
   prng = PRNG.new(0)
   p prng
-  x = RoughPredictor.predict(prng, starters)
-  print_condition(x.to_a.sample, starters)
+  results = RoughPredictor.predict(prng, starters).to_a
+  results_filtered = results.select{|r| judge(r, starters) }
+  print_context results_filtered.last, starters
 end
 
-def print_condition(result, starters)
-  shop, gate = result_to_shopgate(result, starters)
+def print_context(result, starters)
+  context = JudgeContext.from_predictor_result(result, starters)
+  shop, gate = context.shop, context.gate
   (0..N - 2).each do |i|
     puts "shop #{i}: #{shop[i]}"
     puts "gate #{i+2}: #{gate[i+2]} : #{shop[i+2]}"
   end
-  req = requests_list()
-  pp req
-  assign_top_priority_requests(req)
+  pp context.requests
+end
+
+def judge(result, starters)
+  context = JudgeContext.from_predictor_result(result, starters)
+  assign_loop(context)
 end
 
 # gate iの一つ手前でアイテムaを得て少なくともgate jまで保持し続けるという仕事を
@@ -39,21 +45,31 @@ Work.class_eval do
   def range() head..tail end
 end
 
-# 絶対に採用する必要があるworkを採用する
-def assign_top_priority_requests(req)
-  assigner = Assigner.new()
-  req.each do |r|
-    if r.size == 1
-      assigner.assign(r[0])
-      puts "assigned: #{r[0]}"
+# 絶対に採用する必要があるworkを採用することを続ける
+def assign_loop(context)
+  assigner = Assigner.new(context.shop)
+  requests = context.requests.to_a
+  begin
+    updated = false
+    requests.each_with_index do |req, i|
+      next if req == nil
+      c = req.count {|r| assigner.assignable?(r) }
+      return false if c == 0
+      if c == 1
+        assigner.assign(req.find {|r| assigner.assignable?(r) })
+        requests[i] = nil
+        updated = true
+      end
     end
-  end
+  end while updated
+  true
 end
 
 # 仕事の割り当て
 class Assigner
-  def initialize
+  def initialize(bad)
     @assigned = []
+    @bad = bad
   end
   
   def assign(work)
@@ -74,8 +90,13 @@ class Assigner
 
   private
   def assignable0(assigned, work)
-    work.range.all? {|i| covered_num(assigned, i) < M } \
-     and startable_num(assigned, work.head) >= 1
+    work.range.all? {|i| covered_num(assigned, i) < M }\
+      and startable_num(assigned, work.head) >= 1 \
+      and pass_bad_condition(work)
+  end
+
+  def pass_bad_condition(work)
+    work.range.all? {|i| not @bad[i].include?(work.item) }
   end
 
   def exist_similar_longer_work(work)
@@ -101,72 +122,5 @@ class Assigner
     assigned.count {|work| work.range.include?(pos) }
   end
 end
-
-
-# 要求されるworkのリスト
-def requests_list
-  req = []
-  (2..N).each do |i|
-    @gate[i].each do |item|
-      r = []
-      (0..i-2).each do |j|
-        if @shop[j].include?(item)
-          r << Work.new(item, j + 2, i)
-        end
-      end
-      req << r
-    end
-  end
-  req
-end
-
-class Item
-  def initialize(name)
-    @name = name
-  end
-
-  def inspect
-    @name
-  end
-end
-
-def result_to_shopgate(result, starters)
-  shop = []
-  gate = []
-  shop[0] = starters
-  (1..N).each do |i|
-    shop[i] = result.enemies[i-1]
-  end
-  (2..N).each do |i|
-    gate[i] = result.skipped[i-1]
-  end
-  @shop, @gate = subst_item_id(shop, gate)
-end
-
-# デバッグのために、どこのshopで手に入れることができるかを表した名前にする
-def subst_item_id(shop, gate)
-  ref = {}
-  (0..N).each do |i|
-    shop[i].each do |item|
-      ref[item] ||= []
-      ref[item] << i if i <= N - 2
-    end
-  end
-  used = ref.values.each_with_object(Hash.new(0)) {|r, u| u[r] += 1 }
-  count = Hash.new(0)
-  name = {}
-  ref.each do |item, r|
-    n = r.join("")
-    if used[r] > 1 or n == ""
-      n += "_" + ("a".ord + count[r]).chr
-    end
-    name[item] = Item.new(n)
-    count[r] += 1
-  end
-  shop.each {|x| x.map!{|item| name[item] } }
-  gate.each {|x| x and x.map!{|item| name[item] } }
-  [shop, gate]
-end
-
 
 main() if $0 == __FILE__

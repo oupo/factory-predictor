e2ed4c4a4a63d3e91fbbdd35e1a2d632b0517545
oupo <oupo.nejiki@gmail.com>
2013-03-31 18:47:42 +0900

    skippedにchosenと衝突しないすべてのエントリーを出し尽くしたときには空集合を返すようにする変更。無限再帰は回避できたようだが、遅すぎて一向に終わらないのでこの方法はあきらめよう。

--- rough.rb
+++ rough.rb
@@ -18,21 +18,40 @@ class Predictor
   end
 
   def predict0(prng, skipped, chosen)
-    if chosen.length == nParty
-      return Set.new([chosen])
+    while true
+      if chosen.length == nParty
+        return Set.new([chosen])
+      end
+      prngp, x = choose_entry(prng)
+      if x.collides_within?(chosen)
+        # 既に採用済みの場合
+        prng = prngp
+        next
+      elsif skipped =~ all_entries() - similar_entries(chosen)
+        # chosenと衝突しないエントリーを全てskipし尽くしてしまった場合
+        return Set.new()
+      elsif skipped.include?(x)
+        # まったく同じエントリをスキップしたことがある場合
+        prng = prngp
+        next
+      else
+        result1 = predict0(prngp, skipped, chosen + [x])
+        result2 = predict0(prngp, skipped + [x], chosen)
+        return result1 + result2
+      end
     end
-    prngp, x = choose_entry(prng)
-    if x.collides_within?(chosen)
-      # 既に採用済みの場合
-      predict0(prngp, skipped, chosen)
-    elsif skipped.include?(x)
-      # まったく同じエントリをスキップしたことがある場合
-      predict0(prngp, skipped, chosen)
-    else
-      result1 = predict0(prngp, skipped, chosen + [x])
-      result2 = predict0(prngp, skipped + [x], chosen)
-      result1 + result2
-    end
+  end
+
+  def similar_entries(entries)
+    all_entries().find_all {|entry|
+      entry.collides_within?(entries)
+    }
+  end
+end
+
+class Array
+  def =~(other)
+    Set.new(self) == Set.new(other)
   end
 end
 

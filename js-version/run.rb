400f01677f156929239675ba47f9a1a0e3abed19
oupo <oupo.nejiki@gmail.com>
2013-04-09 06:57:38 +0900

    add entries.csv, run.rb, traceur.js
    (traceur.js is copied from revision 5dbc921)

---
+++ js-version/run.rb
@@ -0,0 +1,6 @@
+def sys(cmd) system(cmd) or abort end
+
+sys "traceur --experimental --out compiled.js util.js prng.js factory-helper.js rough.js judge.js hello.js"
+sys "type traceur.js > compiled2.js"
+sys "type compiled.js >> compiled2.js"
+sys "node compiled2.js"

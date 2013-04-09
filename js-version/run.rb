30a053374ac370b872c21d4eb39b0d70116e37da
oupo <oupo.nejiki@gmail.com>
2013-04-10 08:50:41 +0900

    traceur.jsをリポジトリからはずす

--- js-version/run.rb
+++ js-version/run.rb
@@ -1,4 +1,14 @@
+require "open-uri"
+def prepare_traceur_js
+  if not File.exists?("traceur.js")
+    data = open("https://raw.github.com/google/traceur-compiler/master/bin/traceur.js").read
+    File.binwrite "traceur.js", data
+  end
+end
+
 def sys(cmd) system(cmd) or abort end
+
+prepare_traceur_js
 sys "traceur --experimental --out compiled.js util.js prng.js factory-helper.js rough.js judge.js hello.js"
 sys "type traceur.js > compiled2.js"
 sys "type compiled.js >> compiled2.js"

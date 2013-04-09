720b219067b84958ae44ff33363a09b5b82718b5
oupo <oupo.nejiki@gmail.com>
2013-04-09 09:34:26 +0900

    seedを入力するフォーム作成

--- js-version/web.js
+++ js-version/web.js
@@ -1,14 +1,29 @@
+var env;
+
 function main() {
-	var env;
 	await env = FactoryHelper.buildEnv({
 		nParty: 3,
 		nStarters: 6,
 		nBattles: 7,
 		allEntriesURL: "entries.csv"
 	});
-	var result = Predictor.predict(env, new PRNG(0));
-	console.log(result);
 	document.body.innerHTML = `
+		<form action="" onsubmit="return false">
+		seed: <input type="text" id="seed" value="0">
+		<input type="submit" value="実行">
+		</form>
+		<div id="result"></div>
+	`;
+	document.querySelector("form").addEventListener("submit", () => {
+		exec(Number(document.querySelector("#seed").value));
+	}, false);
+}
+
+function exec(seed) {
+	var result = Predictor.predict(env, new PRNG(seed));
+	console.log(result);
+	document.querySelector("#result").innerHTML = `
+		結果: ${result.length} 件
 		<table>
 		<tr><td></td>${Util.iota(env.nBattles).map(i => `<td>${i+1}戦目</td>`).join("")}</tr>
 		${
@@ -24,4 +39,6 @@ function main() {
 		}</table>
 	`;
 }
-window.addEventListener("load", main().then(() => console.log("done"), e => console.log(e)));
+window.addEventListener("load", () => {
+	main().then(() => console.log("done"), e => console.log(e));
+});

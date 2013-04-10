84307d76a69a4cbafa303405c565eaa493d15eb5
oupo <oupo.nejiki@gmail.com>
2013-04-10 09:57:43 +0900

    utils.jsにおいて、メソッドを定義する手続きを用意しておいてそれを使う

--- js-version/util.js
+++ js-version/util.js
@@ -10,123 +10,75 @@ Object.defineProperty(Array.prototype, "flatten", {
 	enumerable: false,
 	writable: true
 });
-
-Object.defineProperty(Array.prototype, "include", {
-	value: function(x) {
-		return this.indexOf(x) >= 0;
-	},
-	configurable: true,
-	enumerable: false,
-	writable: true
+function defineMethod(object, name, func) {
+	Object.defineProperty(object, name, {
+		value: func,
+		configurable: true,
+		enumerable: false,
+		writable: true
+	});
+}
+defineMethod(Array.prototype, "include", function (x) {
+	return this.indexOf(x) >= 0;
 });
 
-Object.defineProperty(Array.prototype, "clone", {
-	value: function() {
-		return this.slice(0);
-	},
-	configurable: true,
-	enumerable: false,
-	writable: true
+defineMethod(Array.prototype, "clone", function () {
+	return this.slice(0);
 });
 
-Object.defineProperty(Array.prototype, "count", {
-	value: function(predicate) {
-		var num = 0;
-		for (var x of this) {
-			if (predicate(x)) num += 1;
-		}
-		return num;
-	},
-	configurable: true,
-	enumerable: false,
-	writable: true
+defineMethod(Array.prototype, "count", function (predicate) {
+	var num = 0;
+	for (var x of this) {
+		if (predicate(x)) num += 1;
+	}
+	return num;
 });
 
-Object.defineProperty(Array.prototype, "diff", {
-	value: function(other) {
-		return this.filter(x => !other.include(x));
-	},
-	configurable: true,
-	enumerable: false,
-	writable: true
+defineMethod(Array.prototype, "diff", function (other) {
+	return this.filter(x => !other.include(x));
 });
 
-Object.defineProperty(Array.prototype, "cap", {
-	value: function(other) {
-		return this.filter(x => other.include(x));
-	},
-	configurable: true,
-	enumerable: false,
-	writable: true
+defineMethod(Array.prototype, "cap", function (other) {
+	return this.filter(x => other.include(x));
 });
 
-Object.defineProperty(Array.prototype, "sortBy", {
-	value: function(func) {
-		var keys = this.map(func);
-		return Util.iota(this.length).sort((a, b) => keys[a] - keys[b]).map(i => this[i]);
-	},
-	configurable: true,
-	enumerable: false,
-	writable: true
+defineMethod(Array.prototype, "sortBy", function (func) {
+	var keys = this.map(func);
+	return Util.iota(this.length).sort((a, b) => keys[a] - keys[b]).map(i => this[i]);
 });
 
-Object.defineProperty(Array.prototype, "minBy", {
-	value: function(keyOf) {
-		var min = null;
-		for (var x of this) {
-			if (min == null || keyOf(x) < keyOf(min)) min = x;
-		}
-		return min;
-	},
-	configurable: true,
-	enumerable: false,
-	writable: true
+defineMethod(Array.prototype, "minBy", function (keyOf) {
+	var min = null;
+	for (var x of this) {
+		if (min == null || keyOf(x) < keyOf(min)) min = x;
+	}
+	return min;
 });
 
-Object.defineProperty(Array.prototype, "maxBy", {
-	value: function(keyOf) {
-		var max = null;
-		for (var x of this) {
-			if (max == null || keyOf(max) < keyOf(x)) max = x;
-		}
-		return max;
-	},
-	configurable: true,
-	enumerable: false,
-	writable: true
+defineMethod(Array.prototype, "maxBy", function (keyOf) {
+	var max = null;
+	for (var x of this) {
+		if (max == null || keyOf(max) < keyOf(x)) max = x;
+	}
+	return max;
 });
 
-Object.defineProperty(Array.prototype, "max", {
-	value: function() {
-		return this.maxBy(x => x);
-	},
-	configurable: true,
-	enumerable: false,
-	writable: true
+defineMethod(Array.prototype, "max", function () {
+	return this.maxBy(x => x);
 });
 
-Object.defineProperty(Array.prototype, "findIndex", {
-	value: function(func) {
-		for (var i = 0; i < this.length; i ++) {
-			if (func(this[i])) return i;
-		}
-		return null;
-	},
-	configurable: true,
-	enumerable: false,
-	writable: true
+defineMethod(Array.prototype, "findIndex", function (func) {
+	for (var i = 0; i < this.length; i ++) {
+		if (func(this[i])) return i;
+	}
+	return null;
 });
 
-Object.defineProperty(Array.prototype, "find", {
-	value: function(func) {
-		for (var x of this) {
-			if (func(x)) return x;
-		}
-		return null;
-	},
-	configurable: true,
-	enumerable: false,
-	writable: true
+defineMethod(Array.prototype, "find", function (func) {
+	for (var x of this) {
+		if (func(x)) return x;
+	}
+	return null;
 });
 
 Object.defineProperty(Array.prototype, "last", {

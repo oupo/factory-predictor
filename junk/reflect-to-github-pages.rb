2fe811f3c3418fe579463b149e8941080869c787
oupo <oupo.nejiki@gmail.com>
2013-04-10 11:29:35 +0900

    反映するときscript src属性にcache対策のqueryをつける

--- junk/reflect-to-github-pages.rb
+++ junk/reflect-to-github-pages.rb
@@ -3,14 +3,26 @@ require_relative "../js-version/compile.rb"
 
 def sys(cmd) puts cmd; system(cmd) or abort end
 
-files = ["simple.html.js"]
+files = ["simple.html"]
 dir = 'c:\users\user\repos\oupo.github.com\factory-predictor'
 
+epoch = Time.now.to_i
+
 files.each do |file|
-  compile file
-  FileUtils.copy compiled_path(file), "#{dir}/compiled"
+  js_file = "#{file}.js"
+  compile js_file
+  FileUtils.copy file, dir
+  FileUtils.copy compiled_path(js_file), "#{dir}/compiled"
+  data = File.binread("#{dir}/#{file}")
+  open("#{dir}/#{file}", "wb") do |f|
+    f.write data.sub(/<script src="compiled\/([^"]+)">/) {
+      "<script src=\"compiled/#{$1}?#{epoch}\">"
+    }
+  end
+
   Dir.chdir dir do
-    sys "git add compiled/#{File.basename(compiled_path(file))}"
+    sys "git add #{file}"
+    sys "git add compiled/#{File.basename(compiled_path(js_file))}"
   end
 end
 

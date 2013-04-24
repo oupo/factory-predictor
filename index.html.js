b8ff248941d33ded320a6b5763dc6ff95c57d71b
oupo <oupo.nejiki@gmail.com>
2013-04-25 08:45:38 +0900

    樹形図ビジュアライゼーション作成

--- index.html.js
+++ index.html.js
@@ -60,13 +60,43 @@ function loadImage(url) {
 
 function exec(seed) {
 	var result = Predictor.predict(gEnv, new PRNG(seed));
-	//var tree = toTree(gEnv, result);
-	var canvas = drawNode(result[0].enemies[0]);
+	var tree = toTree(gEnv, result);
+	//console.log(displayTree(gEnv, tree, 0));
+	var canvas = drawTree(tree, 0);
 	document.querySelector("#result").innerHTML = "";
 	document.querySelector("#result").appendChild(canvas);
 }
 
-function drawNode(entries) {
+function drawTree(node, nest) {
+	const PAD_X = 10, PAD_Y = 50;
+	var [x, children] = node;
+	var canvases = children.map(child => scaleCanvas(drawTree(child, nest + 1), 0.6));
+	var vertex = drawVertex(x);
+	var height = vertex.height + PAD_Y + canvases.map(c => c.height).max()
+	var width = canvases.reduce((r,c) => r + c.width, 0) + (canvases.length - 1) * PAD_X;
+	width = Math.max(width, vertex.width);
+	var ctx = newCanvas(width, height);
+	ctx.drawImage(vertex, (width - vertex.width) / 2, 0);
+	var x = 0;
+	canvases.forEach((child, i) => {
+		ctx.beginPath();
+		ctx.moveTo(width / 2, vertex.height);
+		ctx.lineTo(x + child.width / 2, vertex.height + PAD_Y);
+		ctx.stroke();
+		ctx.drawImage(child, x, vertex.height + PAD_Y);
+		x += child.width + PAD_X;
+	});
+	return ctx.canvas;
+}
+
+function scaleCanvas(canvas, scale) {
+	var w = canvas.width * scale, h = canvas.height * scale;
+	var ctx = newCanvas(w, h);
+	ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, w, h);
+	return ctx.canvas;
+}
+
+function drawVertex(entries) {
 	var s = 32;
 	var [w, h] = [100, 32];
 	var ctx = newCanvas(w, h);
@@ -84,22 +114,26 @@ function drawNode(entries) {
 
 function newCanvas(w, h) {
 	var canvas = document.createElement("canvas");
+	canvas.width = w, canvas.height = h;
 	return canvas.getContext("2d");
 }
 
-function displayTree(env, tree, nest) {
-	var indent = Util.str_repeat("--", nest + 1);
-	var prefix = `${indent} ${nest+1}. `;
+function displayTree(env, node, nest) {
+	var indent = Util.str_repeat("--", nest);
+	var prefix = `${indent} ${nest + 1}. `;
 	var out = "";
-	for (var [x, children] of tree) {
-		out += `${prefix}${x.map(e => e.pokemon).join(" ")}\n`;
-		out += displayTree(env, children, nest + 1);
+	var [x, children] = node;
+	out += `${prefix}${x.map(e => e.pokemon).join(" ")}\n`;
+	for (var child of children) {
+		out += displayTree(env, child, nest + 1);
 	}
 	return out;
 }
 
 function toTree(env, results) {
-	return toTree0(env, results, 0);
+	var nodes = toTree0(env, results, 0);
+	if (nodes.length != 1) throw "nodes.length must == 1";
+	return nodes[0];
 }
 
 function toTree0(env, results, i) {
